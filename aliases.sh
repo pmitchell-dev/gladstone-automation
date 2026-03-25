dafb91f41b78f8e05797ffcebb291ed1aa1246c3
Patrick Mitchell <legendary034@gmail.com>
2026-03-25 14:08:41 -0500

    Auto-sync Wed Mar 25 02:08:41 PM CDT 2026

---
+++ aliases.sh
@@ -0,0 +1,16 @@
+# --- Master Aliases (Synced via GitHub) ---
+
+# The Magic Sync
+alias sync='bash $HOME/scripts/sync_repo.sh'
+
+# Navigation Helpers
+alias sc='cd ~/scripts'
+alias home='cd ~'
+
+# Quick Logs
+alias nlog='tail -f ~/ntfy.log'
+alias slog='tail -f ~/services_manager.log'
+
+# System
+alias lsa='ls -lah'
+alias update='sudo apt update && sudo apt upgrade -y'
