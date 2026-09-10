0cf1ae58914211fa0c5a30f929d70af308f40e62
P Mitchell <Legendary034@gmail.com>
2026-09-09 23:43:04 -0500

    Change default OS user password to 'root'

--- setup_hub.sh
+++ setup_hub.sh
@@ -23,7 +23,7 @@ apt-get install -y jq curl git bc zip cifs-utils python3 smbclient rclone speedt
 if ! id "gladstone" &>/dev/null; then
   echo "👤 Creating 'gladstone' user..."
   useradd -m -s /bin/bash gladstone
-  echo "gladstone:GladstoneTemp123!" | chpasswd
+  echo "gladstone:root" | chpasswd
   usermod -aG sudo gladstone
   echo "gladstone ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/gladstone
   chmod 0440 /etc/sudoers.d/gladstone
